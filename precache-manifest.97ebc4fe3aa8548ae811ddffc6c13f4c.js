72f661264193dde99a65f25c1a161aa8848c3c1e
CHEW TZE YANG RP <19037802@myrp.edu.sg>
2021-11-13 20:21:06 +0800

    Updates

---
+++ precache-manifest.97ebc4fe3aa8548ae811ddffc6c13f4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f76452c2a9ecd855d9066692cad7be",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4571bb2896119a795cdb",
+    "url": "/monsters-rolodex/static/css/main.8d423ed4.chunk.css"
+  },
+  {
+    "revision": "c8e56e1d6cf27670d4ed",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e36c4017ec0df4e6e55",
+    "url": "/monsters-rolodex/static/js/3.7b815fc8.chunk.js"
+  },
+  {
+    "revision": "4571bb2896119a795cdb",
+    "url": "/monsters-rolodex/static/js/main.28b014e2.chunk.js"
+  },
+  {
+    "revision": "8255626bc43072b12186",
+    "url": "/monsters-rolodex/static/js/runtime-main.a6cb5aee.js"
+  }
+]);
